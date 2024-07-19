6ddd34c859537055109864b45e62a4e6c5532e37
MRINMOY MONDAL <mm2922894@gmail.com>
2024-07-20 03:33:13 +0530

    added redux toolkit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -8,10 +8,13 @@ import { Stack } from "expo-router/stack";
 import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import ImageColors, { ImageColorsResult } from "react-native-image-colors";
+import ImageColors from "react-native-image-colors";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import TrackPlayer, { Track, useActiveTrack } from "react-native-track-player";
+import { Provider } from "react-redux";
 import { addTrack, playbackService, setupPlayer } from "rntp-service";
+import { store } from "@/app/store";
+import { ImageColorsResult } from "react-native-image-colors/lib/typescript/types";
 
 export default function Layout() {
   const theme = useAppTheme();
@@ -61,32 +64,34 @@ export default function Layout() {
   }
 
   return (
-    <Material3ThemeProvider
-      settings={{
-        icon: (props: any) => <Ionicons {...props} />,
-      }}
-    >
-      <GestureHandlerRootView
-        style={{ backgroundColor: theme.colors.onBackground }}
-        className="h-full"
+    <Provider store={store}>
+      <Material3ThemeProvider
+        settings={{
+          icon: (props: any) => <Ionicons {...props} />,
+        }}
       >
-        <Stack
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: theme.colors.onBackground,
-            },
-            headerTintColor: theme.colors.onPrimary,
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-            headerTitleAlign: "center",
-            headerShown: false,
-          }}
+        <GestureHandlerRootView
+          style={{ backgroundColor: theme.colors.onBackground }}
+          className="h-full"
         >
-          <Stack.Screen name="index" />
-        </Stack>
-      </GestureHandlerRootView>
-    </Material3ThemeProvider>
+          <Stack
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: theme.colors.onBackground,
+              },
+              headerTintColor: theme.colors.onPrimary,
+              headerTitleStyle: {
+                fontWeight: "bold",
+              },
+              headerTitleAlign: "center",
+              headerShown: false,
+            }}
+          >
+            <Stack.Screen name="index" />
+          </Stack>
+        </GestureHandlerRootView>
+      </Material3ThemeProvider>
+    </Provider>
   );
 }
 
