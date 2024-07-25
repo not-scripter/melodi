d67abd2aafa893daf309c24d5c20071e1cbe940b
MRINMOY MONDAL <mm2922894@gmail.com>
2024-07-25 17:15:50 +0530

    test

--- src/app/(tabs)/playlists.tsx
+++ src/app/(tabs)/playlists.tsx
@@ -1,24 +1,24 @@
 import React, { useState } from "react";
 import { View } from "react-native";
 import { Button } from "react-native-paper";
-// import youtubeDl from "youtube-dl-exec";
+import youtubeDl from "youtube-dl-exec";
 
 export default function Playlists() {
-  const [console, setconsole] = useState<any>();
+  const [yttest, setyttest] = useState<any>();
   async function test() {
-    //   try {
-    //     youtubeDl("https://www.youtube.com/watch?v=6xKWiCMKKJg", {
-    //       dumpSingleJson: true,
-    //       noCheckCertificates: true,
-    //       noWarnings: true,
-    //       preferFreeFormats: true,
-    //       addHeader: ["referer:youtube.com", "user-agent:googlebot"],
-    //     })
-    //       .then((output) => setconsole(output))
-    //       .catch((error) => console.log(error));
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
+    try {
+      youtubeDl("https://www.youtube.com/watch?v=6xKWiCMKKJg", {
+        dumpSingleJson: true,
+        noCheckCertificates: true,
+        noWarnings: true,
+        preferFreeFormats: true,
+        addHeader: ["referer:youtube.com", "user-agent:googlebot"],
+      })
+        .then((output) => setyttest(output))
+        .catch((error) => console.log(error));
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
@@ -26,7 +26,7 @@ export default function Playlists() {
       <Button mode="elevated" onPress={test}>
         Play
       </Button>
-      <View>{console}</View>
+      <View>{yttest}</View>
     </View>
   );
 }
