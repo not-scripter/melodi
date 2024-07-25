fc86de688f9cb8bbef477172e1760c8d3f7f09f2
MRINMOY MONDAL <mm2922894@gmail.com>
2024-07-26 01:43:34 +0530

    handled notification click

--- src/app/(tabs)/playlists.tsx
+++ src/app/(tabs)/playlists.tsx
@@ -1,32 +1,15 @@
-import React, { useState } from "react";
+import { Link } from "expo-router";
+import React from "react";
 import { View } from "react-native";
 import { Button } from "react-native-paper";
-import youtubeDl from "youtube-dl-exec";
 
 export default function Playlists() {
-  const [yttest, setyttest] = useState<any>();
-  async function test() {
-    try {
-      youtubeDl("https://www.youtube.com/watch?v=6xKWiCMKKJg", {
-        dumpSingleJson: true,
-        noCheckCertificates: true,
-        noWarnings: true,
-        preferFreeFormats: true,
-        addHeader: ["referer:youtube.com", "user-agent:googlebot"],
-      })
-        .then((output) => setyttest(output))
-        .catch((error) => console.log(error));
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   return (
     <View>
-      <Button mode="elevated" onPress={test}>
-        Play
-      </Button>
-      <View>{yttest}</View>
+      <Link href="test">
+        <Button mode="elevated">Test</Button>
+      </Link>
+      <View>{}</View>
     </View>
   );
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,23 +1,24 @@
-import { Material3ThemeProvider } from "@/components/providers/Material3ThemeProvider";
-import Ionicons from "@expo/vector-icons/Ionicons";
 import { persistor, store } from "@/app/store";
 import Player from "@/components/Player";
+import { Material3ThemeProvider } from "@/components/providers/Material3ThemeProvider";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import * as Linking from "expo-linking";
+import { useRouter } from "expo-router";
 import { Stack } from "expo-router/stack";
+import { Platform } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { ActivityIndicator } from "react-native-paper";
 import TrackPlayer from "react-native-track-player";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { playbackService } from "rntp-service";
-import * as Linking from "expo-linking";
-import { ActivityIndicator } from "react-native-paper";
-import { useNavigation, useRouter } from "expo-router";
 
 export default function RootLayout() {
   const router = useRouter();
 
   Linking.addEventListener("url", ({ url }) => {
     if (url === "trackplayer://notification.click") {
-      router.push("settings");
+      router.replace("settings");
     }
   });
 
